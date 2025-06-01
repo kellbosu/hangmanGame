6f8186bc586e6af12eee53006a4f165bad392ce1
Brandon <kellb@oregonstate.edu>
2025-05-31 23:47:35 -0400

    Keeps track of guessed letters, spaces left, and displays them both in CLI

--- hangmanGame.py
+++ hangmanGame.py
@@ -1,6 +1,9 @@
 import random
 
-# select a word
+# continue to do this until either game over or win
+game_over = False
+lives = 6
+
 random_words = [
     "lantern", "glimpse", "echo", "velocity", "marble",
     "thread", "plume", "jungle", "ripple", "ember",
@@ -8,27 +11,49 @@ random_words = [
     "breeze", "whistle", "crimson", "shatter", "latch"
 ]
 
+# Selects a random word from list random_words
 game_word = random.choice(random_words)
-
 print(game_word)
 
-# display correct letters 
 # Displays the number of letters in the player's word
 num_letters = (len(game_word))
-spaces = ""
 
+# Stores spaces and correctly guessed letters
+spaces_left = []
 x = range(1, num_letters + 1)
 for n in x:
-    spaces += "_ "
-print(spaces)
+    spaces_left.append("_ ")
 
+# Concatenates  for UI
+def concat_word():
+    working_word = ""
+    for letter in spaces_left:
+        working_word += letter
+    print(working_word)
 
+concat_word()
 # ask for user input to guess a letter
 
+guess = input("guess a letter: ")
+for i, letter in enumerate(game_word):
+    if letter == guess:
+        print(f"Index {i}: {letter}")
+        spaces_left[i] = guess
+concat_word()
 
+
+ 
+
+        
+
+
+
+    
+
+# display correct letters 
 
 # notify when wrong letter is selected
 
 # keep track of lives
 
-# continue to do this until either game over or win
+
